f0f8eed21d7fefcab0c861cc90f70427348df5cf
pglen <peterglen99@gmail.com>
2026-05-18 16:28:28 +0100

    auto checkin

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,4 +4,18 @@ import spelib.spemod
 mod = spelib.spemod.spellencrypt()
 
 def test_one():
-    assert 1
+    assert mod
+    arr = ["Hello", " ", "World"]
+    passx = "1234"
+    enc = mod.enc_dec(True, arr, passx)
+    #print(enc)
+    assert enc == "Inactivenesses Allerie"
+
+def test_two():
+    arr = ["Hello", " ", "World", " ", "1234"]
+    passx = "1234"
+    enc = mod.enc_dec(True, arr, passx)
+    #print(enc)
+    assert enc == "Inactivenesses Allerie 1234"
+
+# EOF
