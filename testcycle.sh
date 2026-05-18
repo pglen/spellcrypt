91958a33fffac2d10db43da5b43678f134e3dea1
pglen <peterglen99@gmail.com>
2026-05-18 13:13:10 +0100

    auto checkin

--- testcycle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#VERBOSE="-v 2"
-echo Diff should be silent
-./spellcrypt.py $VERBOSE -e -f -i orig/$1$1   -o  orig/$1$1$1
-./spellcrypt.py $VERBOSE -d -f -i orig/$1$1$1 -o  orig/$1$1$1$1
-diff  orig/$1$1 orig/$1$1$1$1
-
-
